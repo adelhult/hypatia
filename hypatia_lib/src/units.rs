67c881245a9d3d368f54f846b06d2b2f8c756a69
Jonathan Widén <jonathan.loke.widen@gmail.com>
2022-06-14 12:18:25 +0200

    Added some more tests, added try_convert to Quantity and extracted rusty boilerplate for tests to another function

--- hypatia_lib/src/units.rs
+++ hypatia_lib/src/units.rs
@@ -1,5 +1,6 @@
-use num::rational::Ratio;
 use std::{collections::BTreeMap, fmt, ops};
+
+use num::rational::Ratio;
 
 #[derive(Clone, Debug)]
 struct Quantity(f64, Unit);
@@ -19,19 +20,27 @@ impl Quantity {
     fn normalize(self) -> Self {
         Quantity(self.0 * self.1.0, Unit(1.0, self.1.1))
     }
+    
+    fn try_convert(&self, target_unit: Unit) -> Option<Self> {
+        return if self.1.1 != target_unit.1 {
+            None
+        } else {
+            Some(Quantity(self.0 * self.1.0 / target_unit.0, target_unit))
+        };
+    }
 }
 
 impl ops::Add for Quantity {
     type Output = Option<Self>;
-
+    
     fn add(self, rhs: Self) -> Self::Output {
         let Quantity(mag1, Unit(scale1, powers1)) = self;
         let Quantity(mag2, Unit(scale2, powers2)) = rhs;
-
+        
         if powers1 != powers2 {
             return None;
         }
-
+        
         Some(Quantity(
             // normalize to scale1
             mag1 + (mag2 * scale2 / scale1),
@@ -42,15 +51,15 @@ impl ops::Add for Quantity {
 
 impl ops::Sub for Quantity {
     type Output = Option<Self>;
-
+    
     fn sub(self, rhs: Self) -> Self::Output {
         let Quantity(mag1, Unit(scale1, powers1)) = self;
         let Quantity(mag2, Unit(scale2, powers2)) = rhs;
-
+        
         if powers1 != powers2 {
             return None;
         }
-
+        
         Some(Quantity(
             // normalize to scale1
             mag1 - (mag2 * scale2 / scale1),
@@ -61,22 +70,22 @@ impl ops::Sub for Quantity {
 
 impl ops::Mul for Quantity {
     type Output = Self;
-
+    
     fn mul(self, rhs: Self) -> Self::Output {
         let Quantity(mag1, unit1) = self;
         let Quantity(mag2, unit2) = rhs;
-
+        
         Quantity(mag1 * mag2, unit1 * unit2)
     }
 }
 
 impl ops::Div for Quantity {
     type Output = Self;
-
+    
     fn div(self, rhs: Self) -> Self::Output {
         let Quantity(mag1, unit1) = self;
         let Quantity(mag2, unit2) = rhs;
-
+        
         Quantity(mag1 / mag2, unit1 / unit2)
     }
 }
@@ -93,9 +102,9 @@ impl fmt::Display for Unit {
         } else {
             format!("({}x) ", self.0)
         };
-
-        if self.1.is_empty() {return write!(f, "{}", magnitude)}
-
+        
+        if self.1.is_empty() { return write!(f, "{}", magnitude); }
+        
         let positive = self
             .1
             .iter()
@@ -111,12 +120,12 @@ impl fmt::Display for Unit {
             .collect::<Vec<String>>()
             .join("");
         
-        let pos_str = if positive.is_empty() {"1".into()} else {positive};
-
+        let pos_str = if positive.is_empty() { "1".into() } else { positive };
+        
         let negative = self
             .1
             .iter()
-            .filter(|(_, ratio)| *ratio < &Ratio::new(0i32,1i32))
+            .filter(|(_, ratio)| *ratio < &Ratio::new(0i32, 1i32))
             .map(|(base_unit, ratio)| {
                 if *ratio == Ratio::new(-1i32, 1i32) {
                     // if we have m^1, just display m
@@ -128,12 +137,12 @@ impl fmt::Display for Unit {
             .collect::<Vec<String>>()
             .join("");
         
-        let unit_str = if (negative.is_empty()) {
+        let unit_str = if negative.is_empty() {
             pos_str
         } else {
             format!("{}/{}", pos_str, negative)
         };
-
+        
         write!(f, "{}{}", magnitude, unit_str)
     }
 }
@@ -149,13 +158,13 @@ impl fmt::Display for BaseUnit {
 
 impl ops::Mul for Unit {
     type Output = Self;
-
+    
     fn mul(self, rhs: Self) -> Self {
         let Unit(scale1, pow1) = self;
         let Unit(scale2, pow2) = rhs;
-
+        
         let scale_res = scale1 * scale2;
-
+        
         let powers_res = pow1
             .keys()
             .chain(pow2.keys())
@@ -165,20 +174,20 @@ impl ops::Mul for Unit {
                 (base.clone(), exp)
             })
             .collect();
-
+        
         Self(scale_res, powers_res)
     }
 }
 
 impl ops::Div for Unit {
     type Output = Self;
-
+    
     fn div(self, rhs: Self) -> Self {
         let Unit(scale1, pow1) = self;
         let Unit(scale2, pow2) = rhs;
-
+        
         let scale_res = scale1 / scale2;
-
+        
         let powers_res = pow1
             .keys()
             .chain(pow2.keys())
@@ -188,7 +197,7 @@ impl ops::Div for Unit {
                 (base.clone(), exp)
             })
             .collect();
-
+        
         Self(scale_res, powers_res)
     }
 }
@@ -198,7 +207,7 @@ mod tests {
     use super::*;
     use lazy_static::lazy_static;
     use std::collections::HashMap;
-
+    
     lazy_static! {
         static ref BASE_UNITS: HashMap<char, BaseUnit> = HashMap::from([
             ('m', BaseUnit("meter".to_string(), "m".to_string())),
@@ -206,6 +215,10 @@ mod tests {
             ('s', BaseUnit("second".to_string(), "s".to_string()))
         ]);
         static ref UNITS: HashMap<char, Unit> = HashMap::from([
+            (
+                '0',
+                Unit(1.0, [].into())
+            ),
             (
                 'm',
                 Unit(
@@ -241,23 +254,44 @@ mod tests {
             )
         ]);
     }
-
+    
+    fn unit(c: char) -> Unit {
+        UNITS.get(&c).unwrap().clone()
+    }
+    
     #[test]
-    fn basic_arithmetic() { 
-        let m = Quantity(10_000.0, UNITS.get(&'g').unwrap().clone());
-        let l = Quantity(1.0, UNITS.get(&'m').unwrap().clone());
-        let t = Quantity(4.0, UNITS.get(&'s').unwrap().clone());
-        let f = Quantity(20.0, UNITS.get(&'N').unwrap().clone());
+    fn simple_formatting() {
+        let ten = Quantity(10.0, unit('0'));
+        let five_seconds = Quantity(5.0, unit('s'));
+        let div = ten.clone() / five_seconds.clone();
+        
+        assert_eq!(ten.to_string(), "10");
+        assert_eq!(five_seconds.to_string(), "5 s");
+        assert_eq!(div.to_string(), "2 1/s")
+    }
+    
+    #[test]
+    fn basic_arithmetic() {
+        let m = Quantity(10_000.0, unit('g'));
+        let l = Quantity(1.0, unit('m'));
+        let t = Quantity(4.0, unit('s'));
+        let f = Quantity(20.0, unit('N'));
         
         assert_eq!(&l.to_string(), "1 m");
         assert_eq!(&m.to_string(), "10000 g");
         assert_eq!(&f.to_string(), "20 (1000x) gm/s^2");
-        assert_eq!(Quantity(1.0, Unit(1.0, [].into())).to_string(), "1");
-        dbg!(&f.clone().normalize().to_string());
-
+        assert_eq!(&f.clone().normalize().to_string(), "20000 gm/s^2");
+        assert_eq!(&f
+            .clone()
+            .normalize()
+            .try_convert(unit('N'))
+            .unwrap()
+            .to_string(), "20 (1000x) gm/s^2");
+        assert!(&f.try_convert(unit('s')).is_none());
+        
         // 10 000 g * 1 m / (4s*4s) + 20 N = 625 gm/s^2 + 20 000 gm/s^2 = 20625 gm/s^2
         let result = m * l / (t.clone() * t) + f;
-
+        
         assert_eq!(result.unwrap().to_string(), "20625 gm/s^2");
     }
 }
