1e1df078e64e567f3ce163ef926e55a6598a3ca2
Eli Adelhult <hello@eli.nu>
2022-07-19 21:23:06 +0200

    Add vite react template

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <div className="App">
+      <h1>Hypatia</h1>
+      TODO: link with WASM build
+    </div>
+  )
+}
+
+export default App
