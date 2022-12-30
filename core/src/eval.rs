0d344e3f8f88fd671e6be612a9a214422a09ffb7
Eli Adelhult <hello@eli.nu>
2022-12-30 17:19:05 +0100

    Simplify the format unit function a lot

--- core/src/eval.rs
+++ core/src/eval.rs
@@ -463,72 +463,57 @@ pub fn eval((expr, _): &Spanned<Expr>, env: &mut Environment) -> Result<Value, E
 /// Given a Quantity get the best matching unit to display the quantity as.
 /// Returns a new quantity which might be rescaled if there is no perfect match and
 /// long and short name of the unit.
-pub fn format_unit(
-    quantity: Quantity,
-    env: &Environment,
-    ) -> (Quantity, (String, Option<String>)) {
+pub fn format_unit(quantity: Quantity, env: &Environment) -> (Quantity, (String, Option<String>)) {
     let Quantity { number, unit } = &quantity;
     let Unit(scale, base_units) = unit;
 
     let matches = env.get_unit_names(&base_units);
 
-    // Compare the scale of this unit with the scale used in our Quantity, we pick
-    // the unit with the lowest difference in scale as our prefered unit.
-    // TODO: Rewrite this. This code ended up super imperative and kinda ugly. It was
-    // hard to use util methods like min_by_key on Number since it lacks the Ord trait.
-    let mut best_diff = None;
-    let mut best_match = None;
+    // Compare the scale of this unit with the scale used in our Quantity,
+    // is there a named unit with the same scale?
 
-    for (long_name, short_name) in matches {
+    let unit_name = matches.iter().find_map(|unit_name @ (long_name, _)| {
         let Ok(Unit(other_scale, _)) = env.get_unit(&long_name) else {
-            continue;
+            return None;
         };
 
-        // Compare the scale of the unit found in our quantity with
-        // all possible matches
         let diff = Number::abs(other_scale - scale.clone());
 
-        // Save the one with the smallest difference so far.
-        if let Some(best_diff_value) = best_diff.clone() {
-            if diff < best_diff_value {
-                best_diff = Some(diff.clone());
-                best_match = Some((long_name, short_name));
-            }
-        } else {
-            best_diff = Some(diff.clone());
-            best_match = Some((long_name, short_name));
+        if diff != Number::zero() {
+            return None;
         }
 
-        if diff == Number::zero() {
-            break;
-        }
-    }
+        Some(unit_name)
+    });
 
-    let Some((ref long_name, _)) = best_match else {
+    match unit_name {
+        Some(names @ (ref long_name, _)) => {
+            let Unit(target_scale, _) = env.get_unit(&long_name).unwrap();
+
+
+            // Now, we might need to rescale the original quantity to fit we the unit
+            // that we have selected.
+            let rescaled_quantity = Quantity {
+                number: number.clone() * scale.clone() / target_scale.clone(),
+                unit: Unit(target_scale, base_units.clone()),
+            };
+
+            (rescaled_quantity, names.clone())
+        }
+
         // If we did not find a matching named unit, just rescale the quantity and present it in base units
         // For example, instead of Quantity(2, Unit(1337, meter * second))
         //                      -> Quantity(2 * 1337, Unit( 1, meter * second)
         //                      -> "2674000  m * s"
-        let rescaled_unit = unit.clone().rescaled(Number::one() / scale.clone());
-        let rescaled_quantity = Quantity {number: number.clone() * scale.clone(), unit: rescaled_unit.clone()};
-        return (
-                rescaled_quantity,
-            (format!("{rescaled_unit}"), None)
-        );
-    };
-
-    // If we instead found a match. Then the best matching unit will be our target, so, let's
-    // rescale the provided quantity to fit the unit
-    let Unit(target_scale, _) = env.get_unit(&long_name).unwrap();
-
-    // Now, we might need to rescale the original quantity to fit we the unit
-    // that we have selected.
-    let rescaled_quantity = Quantity {
-        number: number.clone() * scale.clone() / target_scale.clone(),
-        unit: Unit(target_scale, base_units.clone()),
-    };
-
-    (rescaled_quantity, best_match.unwrap())
+        _ => {
+            let rescaled_unit = unit.clone().rescaled(Number::one() / scale.clone());
+            let rescaled_quantity = Quantity {
+                number: number.clone() * scale.clone(),
+                unit: rescaled_unit.clone(),
+            };
+            (rescaled_quantity, (format!("{rescaled_unit}"), None))
+        }
+    }
 }
 
 fn eval_block(expressions: &Vec<Spanned<Expr>>, env: &mut Environment) -> Result<Value, Error> {
